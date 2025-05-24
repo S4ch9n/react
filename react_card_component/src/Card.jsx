215c05538fe11d7cbc55044c856573bfbf9c8508
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2025-05-24 16:09:44 +0530

    react card component

---
+++ react_card_component/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card(){
+  return(
+    <div className="card">
+      <img className="card-image" src="./apple.jpg" alt="profile-picture" ></img>
+      <h2 className="card-title">Coding ninja</h2>
+      <p className="card-text">Student praticing coding</p>
+    </div>
+  );
+}
+
+export default Card
